3109a81ef288238fd4639e18734c29fc995fefbb
klynner33 <113389068+klynner33@users.noreply.github.com>
2026-01-09 14:38:51 -0600

    Added images and responsive styling for main screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
 
     return (
       <div className="footer">
-        <p className="copyright">© 2025 Mystical Math. All rights reserved.</p>
+        <p className="copyright">© 2026 Mystical Math. All rights reserved.</p>
         <p className="credit">Game ideas and testing by Lettie, Grace, & Miles</p>
         <p className="credit">Characters and items by TrulyMalicious — CC-BY 4.0</p>
       </div>
